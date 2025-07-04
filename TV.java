3bd942ff89aa2589778aabed57875e0213e84c47
jawaher94 <157244890+jawaher94@users.noreply.github.com>
2025-07-04 21:23:28 +0300

    Create TV.java

---
+++ TV.java
@@ -0,0 +1,9 @@
+class TV implements Device {
+    public void turnOn() {
+        System.out.println("TV is turned ON.");
+    }
+
+    public void turnOff() {
+        System.out.println("TV is turned OFF.");
+    }
+}
