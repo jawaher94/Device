ad2ae1db46487b1ffdf53cf3b6398e7cf528fc7c
jawaher94 <157244890+jawaher94@users.noreply.github.com>
2025-07-04 21:25:26 +0300

    Create Oven.java

---
+++ Oven.java
@@ -0,0 +1,9 @@
+class Oven implements Device {
+    public void turnOn() {
+        System.out.println("Oven is heating up.");
+    }
+
+    public void turnOff() {
+        System.out.println("Oven is off.");
+    }
+}
