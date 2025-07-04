31e1526df176aeb5a099507ae7da7de74b318971
jawaher94 <157244890+jawaher94@users.noreply.github.com>
2025-07-04 21:24:09 +0300

    Create AirConditioner.java

---
+++ AirConditioner.java
@@ -0,0 +1,9 @@
+class AirConditioner implements Device {
+    public void turnOn() {
+        System.out.println("AC is turned ON.");
+    }
+
+    public void turnOff() {
+        System.out.println("AC is turned OFF.");
+    }
+}
